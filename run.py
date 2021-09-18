c49a41e651d08d9d2170d185f08656b239ae266f
caribbeanyute <smithcleon@gmail.com>
2021-09-17 22:50:04 -0500

    use port from environment variables

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 #! /usr/bin/env python
+ import os
+port = int(os.environ['PORT']) if os.environ['PORT'] else 8084
 from app import app
-app.run(debug=True,host="0.0.0.0",port=8084)
+app.run(debug=True,host="0.0.0.0",port=port)
